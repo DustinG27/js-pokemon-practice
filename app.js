f2b53136d622ef6870942f3a2aa701841b6ca831
dustin george <DustinG27@gmail.com>
2021-08-05 22:02:00 -0400

    added basic css

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const container = document.querySelector('#container');
 const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 
 // for loop for x number to be placed in pokemon link
-for(let i = 1; i < 151; i++) {
+for(let i = 1; i < 901; i++) {
 //creating pokemon div to wrap elements, a label span for text, and img element for img
 const pokemon = document.createElement('div');
+pokemon.classList.add('pokemon');
 const label = document.createElement('span');
-label.innerText = `${i}`;
+label.innerText = `#${i}`;
 const newImg = document.createElement('img');
 // creating the img element and completing the baseURL
 newImg.src = `${baseURL}${i}.png`;
